6ebb8af92d708485ca7487a39e5a4f09db700912
Lynette <lyna8907@gmail.com>
2018-03-16 10:42:38 -0500

    added not null to album tables

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-artist VARCHAR(50),
-name VARCHAR(50),
-release_date INT UNSIGNED,
-sales FLOAT,
+artist VARCHAR(50) NOT NULL,
+name VARCHAR(50) NOT NULL,
+release_date INT UNSIGNED NOT NULL,
+sales FLOAT NOT NULL,
 genre VARCHAR(50),
 PRIMARY KEY(id)
-)
+);
