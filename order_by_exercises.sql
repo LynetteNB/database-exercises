940a729a26b8150e3d0acc550ae0ffde2bb0a500
Lynette <lyna8907@gmail.com>
2018-03-20 10:05:46 -0500

    added order by to all employees with last name e

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,6 @@
 USE employees;
 SELECT first_name, last_name, gender FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY  last_name DESC, first_name DESC;
-SELECT last_name FROM employees WHERE last_name LIKE 'E%';
+SELECT last_name FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
 # Employees hired in the 90s
 SELECT hire_date FROM employees WHERE hire_date LIKE '199%';
 # Employees born on Christmas
@@ -8,9 +8,9 @@ SELECT birth_date FROM employees WHERE birth_date LIKE '%12-25';
 # Employees with a q in their last name
 SELECT last_name FROM employees WHERE last_name LIKE '%q%';
 # Employees whose last name starts or ends with E
-SELECT last_name FROM employees WHERE last_name LIKE '%e' or last_name LIKE 'e%' ORDER BY emp_no DESC;
+SELECT last_name FROM employees WHERE last_name LIKE '%e' or last_name LIKE 'e%' ORDER BY emp_no;
 # Employees whose last name starts AND ends with E
-SELECT last_name FROM employees WHERE last_name LIKE '%e' and last_name LIKE 'e%';
+SELECT last_name FROM employees WHERE last_name LIKE '%e' and last_name LIKE 'e%' ORDER BY emp_no;
 # Employees hired in the 90s and born on Christmas
 SELECT first_name, last_name, hire_date, birth_date FROM employees WHERE hire_date LIKE '199%' and birth_date LIKE '%12-25' ORDER BY hire_date DESC, birth_date DESC;
 # Employees with a q in their last name but not qu
